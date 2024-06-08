912025dda910d255bdab85cf4835dd83994bb761
bajrang <bajrangdangi937@gmail.com>
2024-06-08 10:41:31 +0530

    login page

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App(){
 
   return (
-    <div className="text-3xl font-bold text-green-800">Bhai tera superstarS</div>
+    <div className="text-3xl font-bold text-green-800"></div>
   )
 }
 
